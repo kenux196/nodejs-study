e33e2e31707b49008e3f14c054952574f377554e
skyun <kenux.yun@gmail.com>
2021-06-11 00:51:42 +0900

    Transactional & RollBack

--- MessageAppBE/messages/src/main/java/app/sample/messages/service/MessageService.java
+++ MessageAppBE/messages/src/main/java/app/sample/messages/service/MessageService.java
@@ -5,18 +5,32 @@ import app.sample.messages.filter.annotation.SecurityCheck;
 import app.sample.messages.repository.JDBCMessageRepository;
 import app.sample.messages.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class MessageService {
 
-    private final JDBCMessageRepository repository;
+    private final JDBCMessageRepository jdbcMessageRepository;
     private final MessageRepository messageRepository;
 
+    public Message saveByJDBC(String text) {
+        return jdbcMessageRepository.saveMessage(new Message(text));
+    }
+
     @SecurityCheck
+    @Transactional(noRollbackFor = { UnsupportedOperationException.class })
     public Message save(String text) {
-//        return repository.saveMessage(new Message(text));
-        return messageRepository.save(new Message(text));
+        Message savedMessage = messageRepository.save(new Message(text));
+        log.debug("New message[id={}] saved", savedMessage.getId());
+        updateStatistics();
+        return savedMessage;
+    }
+
+    private void updateStatistics() {
+        throw new UnsupportedOperationException("This method is not implemented yet");
     }
 }
