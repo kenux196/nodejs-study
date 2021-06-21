d4c2e9f7fb86d11d13054552d648e6fa5d2edfa8
skyun <kenux.yun@gmail.com>
2021-06-21 15:16:10 +0900

    Add todo project & init

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>  
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoFooterVue from './components/TodoFooter.vue'
+import TodoHeaderVue from './components/TodoHeader.vue'
+import TodoInputVue from './components/TodoInput.vue'
+import TodoListVue from './components/TodoList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'TodoHeader': TodoHeaderVue,
+    'TodoInput': TodoInputVue,
+    'TodoList': TodoListVue,
+    'TodoFooter': TodoFooterVue
+  }
+}
+</script>
+
+<style>
+
+</style>
