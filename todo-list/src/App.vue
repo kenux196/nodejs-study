1111819bdd0d458f758d6b59bff87bbd860bda74
skyun <kenux.yun@gmail.com>
2021-06-30 00:15:17 +0900

    Apply eslint

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -3,31 +3,31 @@
     <h1>To-Do List</h1>
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
-        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+        <to-do-item :id="item.id" :label="item.label" :done="item.done"></to-do-item>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import ToDoItem from "./components/ToDoItem.vue";
-import uniqueId from 'lodash.uniqueid'
+import ToDoItem from './components/ToDoItem.vue';
+import uniqueId from 'lodash.uniqueid';
 
 export default {
-  name: "App", // component 이름
-  components: { 
+  name: 'App', // component 이름
+  components: {
     // 로컬 컴포넌트 등록
-    ToDoItem
-  }
+    ToDoItem,
+  },
   data() {
     return {
       ToDoItems: [
         { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
         { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
         { id: uniqueId('todo-'), label: 'Have fun', done: true },
-        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
-      ]
-    }
+        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false },
+      ],
+    };
   },
 };
 </script>
